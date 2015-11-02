77c7452c96d8e1322c930ab22210ae5855748253
Celery Qin <celerysoft@gmail.com>
2015-11-02 15:18:42 +0800

    deprecate GeometryUtil class

--- app/src/main/java/com/qingdao/shiqu/arcgis/utils/GeometryUtil.java
+++ app/src/main/java/com/qingdao/shiqu/arcgis/utils/GeometryUtil.java
@@ -9,7 +9,9 @@ import com.esri.core.geometry.Polyline;
 
 /**
  * Geometry工具类
+ * @deprecated ESRI已经提供类似接口，不需要手动实现，白干了
  */
+@Deprecated
 public class GeometryUtil {
 
     /** 将几何对象生成wkt字符串 **/
@@ -20,10 +22,10 @@ public class GeometryUtil {
 
         String geoString = "";
         Geometry.Type type = geometry.getType();
-        if ("Point".equals(type.name())) {
+        if (Geometry.Type.POINT.equals(type)) {
             Point pt = (Point) geometry;
             geoString = type.name() + "(" + pt.getX() + " " + pt.getY() + ")";
-        } else if ("Polygon".equals(type.name()) || "Polyline".equals(type.name())) {
+        } else if (Geometry.Type.POLYGON.equals(type) || Geometry.Type.POLYLINE.equals(type)) {
             MultiPath multiPath = (MultiPath) geometry;
             geoString = type.name() + "(" + "";
             int pathCount = multiPath.getPathCount();
@@ -38,15 +40,15 @@ public class GeometryUtil {
                 geoString += tempString + ",";
             }
             geoString = geoString.substring(0, geoString.length()-1) + ")";
-        } else if ("Envelope".equals(type.name())) {
+        } else if (Geometry.Type.ENVELOPE.equals(type)) {
             Envelope env = (Envelope) geometry;
             geoString = type.name() + "(" + env.getXMin() + "," + env.getYMin() + "," + env.getXMax() + "," + env.getYMax() + ")";
-        } else if ("MultiPoint".equals(type.name())) {
+        } else if (Geometry.Type.MULTIPOINT.equals(type)) {
             // TODO 补全MultiPoint的方法
         } else{
             geoString = null;
         }
-        return geoString;
+         return geoString;
     }
 
     /**
@@ -60,15 +62,15 @@ public class GeometryUtil {
         Geometry geometry = null;
         String headStr = wkt.substring(0, wkt.indexOf("("));
         String tempString = wkt.substring(wkt.indexOf("(") + 1, wkt.lastIndexOf(")"));
-        if (headStr.equals("Point")) {
+        if (headStr.equals(Geometry.Type.POINT.name())) {
             String[] values = tempString.split(" ");
             geometry = new Point(Double.valueOf(values[0]), Double.valueOf(values[1]));
-        } else if(headStr.equals("Polyline") || headStr.equals("Polygon")){
+        } else if(headStr.equals(Geometry.Type.POLYGON.name()) || headStr.equals(Geometry.Type.POLYLINE.name())){
             geometry = parseWkt(tempString, headStr);
-        } else if(headStr.equals("Envelope")){
+        } else if(headStr.equals(Geometry.Type.ENVELOPE.name())){
             String[] extents = tempString.split(",");
             geometry = new Envelope(Double.valueOf(extents[0]), Double.valueOf(extents[1]), Double.valueOf(extents[2]), Double.valueOf(extents[3]));
-        } else if(headStr.equals("MultiPoint")){
+        } else if(headStr.equals(Geometry.Type.MULTIPOINT.name())){
             // TODO 补全MultiPoint的方法
         } else{
             return null;
@@ -88,7 +90,7 @@ public class GeometryUtil {
         }
         Point startPoint;
         MultiPath path;
-        if (type.equals("Polyline")) {
+        if (type.equals(Geometry.Type.POLYLINE.name())) {
             path = new Polyline();
         } else {
             path = new Polygon();
